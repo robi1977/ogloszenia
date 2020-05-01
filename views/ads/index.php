3c87e105f3fe6bfa71970fc45a8982a13bc47e42
Rafal Nowak <rafal.robi.nowak@gmail.com>
2020-05-01 13:25:26 +0200

    aktualizacja widoku z ogoszeniami o link w postaci przycisku o dodawanie ogoszenia

--- views/ads/index.php
+++ views/ads/index.php
@@ -1,3 +1,6 @@
+<?php if(isset($_SESSION['is_logged_in'])) : ?>
+  <a class="btn btn-success btn-add-ad" href="<?php echo ROOT_PATH; ?>ads/add">Dodaj nowe ogłoszenie</a>
+<?php endif; ?>
 <?php foreach($model as $item): ?>
   <div class="well">
     <h3><?php echo $item['title']; ?></h3>
