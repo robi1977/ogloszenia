f2c520743aa209a08d58ea610c8b34accb65999a
Rafal Nowak <rafal.robi.nowak@gmail.com>
2020-05-01 14:53:05 +0200

    dodano nowa metod do modelu 'add'

---
+++ views/ads/add.php
@@ -0,0 +1,19 @@
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Dodaj nowe ogłoszenie</h3>
+  </div>
+  <div class="panel-body">
+    <form method="POST" action="<?php echo ROOT_URL; ?>ads/create">
+      <div class="form-group">
+        <label>Tytuł</label>
+        <input type="text" name="title" class="form-control" />
+      </div>
+      <div class="form-group">
+        <label>Treść</label>
+        <textarea name="content" class="form-control"></textarea>
+      </div>
+      <input class="btn btn-primary" name="submit" type="submit" value="Dodaj"/>
+      <a class="btn btn-danger" href="<?php echo ROOT_PATH; ?>ads">Anuluj</a>
+    </form>
+  </div>
+</div>
