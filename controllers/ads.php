725972017bc51d4a12166008ed6fff848f1bc7de
Rafal Nowak <rafal.robi.nowak@gmail.com>
2020-05-01 14:27:44 +0200

    dodanie dwóch nowych metod do kontrolera 'asd'

--- controllers/ads.php
+++ controllers/ads.php
@@ -8,4 +8,27 @@ class AdsController extends Controller{
   protected function getName(){
     return 'ads';
   }
+
+  public function create(){
+    if(!isset($_SESSION['is_logged_in'])){
+      header('Location: '.ROOT_URL.'ads');
+    }
+
+    $model = new Ad();
+    if ($model->add()){
+      Message::setMsg("Ogłoszenie poprawnie dodane", "success");
+      $this->redirect('ads');
+    } else {
+      Message::setMsg("Nie udało się dodać ogłoszenia", "error");
+      $this->redirect('ads','add');
+    }
+  }
+
+  protected function add(){
+    if(!isset($_SESSION['is_logged_in'])){
+      header('Location: '.ROOT_URL.'ads');
+    }
+
+    $this->returnView('add',true);
+  }
 }
